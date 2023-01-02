11805722402b9b337b7d05a025782d72eb498093
GleidsonLM <gleidsonlm@gmail.com>
2023-01-02 14:51:49 -0300

    add: Create user controller test, it works

--- src/modules/users/useCases/createUser/CreateUserController.test.ts
+++ src/modules/users/useCases/createUser/CreateUserController.test.ts
@@ -1,17 +1,53 @@
 import request from 'supertest';
+import { Connection, createConnection, createQueryBuilder, getRepository } from 'typeorm';
 import { app } from '../../../../app';
+import { User } from '../../entities/User';
+import { ICreateUserDTO } from './ICreateUserDTO';
 
-jest.useFakeTimers();
+let connection: Connection
+beforeAll(async () => {
+  connection = await createConnection();
+  await connection.runMigrations();
+});
+
+afterAll(async () => {
+  await connection.dropDatabase()
+  await connection.close();
+});
 
 describe('User Create', () => {
-  it('should create user and return 201 empty response', async () => {
-    await request(app)
+
+  it('should be able to create a user', async () => {
+    const createUserDTO:ICreateUserDTO = {
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'Password.42'
+    }
+    const response = await request(app)
     .post("/api/v1/users")
-    .send({
-      name:"Full Name",
-      email:"full.name@mail.com",
-      password:"secret"
-    })
-    .expect(201)
+    .send(createUserDTO)
+
+    const userRepository = getRepository(User)
+    const user = await userRepository.findOneOrFail({ where: { email: createUserDTO.email}})
+    
+    expect(response.status).toBe(201);
+    expect(response.body.json).toBeUndefined;
+    expect(user.email).toBe(createUserDTO.email);
+  })
+
+  it('should not be able to create a user with the same email', async () => {
+    const createUserDTO:ICreateUserDTO = {
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'Password.42'
+    }
+
+    const secondResponse = await request(app)
+    .post("/api/v1/users")
+    .send(createUserDTO)
+
+    expect(secondResponse.status).toBe(400);
+    expect(secondResponse.body.message)
+    .toEqual('User already exists')
   })
 })
